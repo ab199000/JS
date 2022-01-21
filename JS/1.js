196ed3a120aeded0f3eabd654e2edfafe643ed27
ab199000 <97779753+ab199000@users.noreply.github.com>
2022-01-21 11:27:10 +0300

    Add files via upload

---
+++ JS/1.js
@@ -0,0 +1,8 @@
+let x1 = 2
+let y1 = 3
+
+let x2 = 10
+let y2 = 5
+
+let S = Math.abs((x1 - x2)*(y1-y2))
+console.log(S)
---
+++ JS/3.js
@@ -0,0 +1,7 @@
+let n = -300 / 2
+let m = -10000 / 2
+
+
+let b = Math.round(Math.random()*Math.abs(n+m) + Math.min(n,m))
+
+console.log(b * 2 -1)
