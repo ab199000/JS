b51be8b1b16bfb4b159c6574ff8c05ef92927031
ab199000 <97779753+ab199000@users.noreply.github.com>
2022-01-27 20:40:57 +0300

    2

--- ЛР_3/2.js
+++ ЛР_3/2.js
@@ -5,5 +5,5 @@ let newSurname = surname.toLowerCase();
 normal_name = newName[0].toUpperCase() + newName.slice(1);
 normal_surname = newSurname[0].toUpperCase() + newSurname.slice(1);
 console.log(normal_name, normal_surname);
-name === normal_name ? console.log("Изменений в имени не произошло"):console.log("Изменения в имени произошли");
-surname === normal_surname ? console.log("Изменений в фамилии не произошло"):console.log("Изменения в фамилии произошли");
+name === normal_name ? console.log("Изменений в имени не было") : console.log("В имени произошли изменения");
+surname === normal_surname ? console.log("Изменений в фамилии не было") : console.log("В фамилии произошли изменения");
