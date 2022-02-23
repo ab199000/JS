f35163f11486f99656707e10ef0df4370e97d151
ab199000 <97779753+ab199000@users.noreply.github.com>
2022-02-23 23:08:57 +0300

    2 exesise

--- дз 2/1.js
+++ дз 2/1.js
@@ -3,15 +3,21 @@ let newArr = [1, 2, 'три', 'четыре',4,6];
 let newObject = {}
 let filterArr = [];
 function getArrObjects(arr){
+    
     for (let i = 0; i < arr.length; i++) {
-        newObject = {
+            newObject= {
             value: arr[i],
             label: arr[i]
         }
         filterArr.push(newObject)
     }
+    // console.log("value" in filterArr[1])
     return filterArr
 }
+
+// console.log("value" in filterArr[1])     не обращать внимания
+// console.log(filterArr)                   кое что проверял
+
 let country = [
   {
     value: "Mr",
@@ -42,4 +48,6 @@ let country = [
     document.body.append(select)
     return select
 }
-getSelect(country,"Gg")
+// getSelect(country,"Gg")   //раскоментить для первого задания
+getArrObjects(newArr)  //для второго
+getSelect(filterArr)   //для второго
